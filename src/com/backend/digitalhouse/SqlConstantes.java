50a8cbd8b77bfc93e9235b36ab9d14a8241e470a
vale0912 <valegb.981209@gmail.com>
2023-08-08 16:18:07 -0500

    commit de prueba

---
+++ src/com/backend/digitalhouse/SqlConstantes.java
@@ -0,0 +1,13 @@
+package com.backend.digitalhouse;
+
+public class SqlConstantes {
+
+    public static final String CREATE= "DROP TABLE IF EXISTS CUENTAS; CREATE TABLE CUENTAS (ID INT PRIMARY KEY AUTO_INCREMENT, NOMBRE VARCHAR(100) NOT NULL, NUMERO_CUENTA INT NOT NULL, SALDO NUMERIC(10, 2) NOT NULL)";
+
+    public static final String INSERT = "INSERT INTO CUENTAS (NOMBRE, NUMERO_CUENTA, SALDO) VALUES (?,?,?)";
+
+    public static final String UPDATE= "UPDATE CUENTAS SET SALDO = ? WHERE NUMERO_CUENTA = ?";
+
+    public static final String SELECT= "SELECT * FROM CUENTAS WHERE ID = ?";
+
+}
